e35e23e62fac2a1d389871d4b9025d22d083e00d
Jonathan Lipsky <lokidog2006@yahoo.com>
2022-02-07 17:04:48 -0600

    cleaned up code - ApiClient

--- app/src/main/java/com/example/googlemapsapp/api/ApiClient.kt
+++ app/src/main/java/com/example/googlemapsapp/api/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    var BASE_URL:String="http://localhost/posts/"
+    private const val BASE_URL = "http://localhost/posts/"
 
     fun apiClient(): Retrofit{
 
@@ -16,7 +16,7 @@ object ApiClient {
             .create()
 
         val interceptor = HttpLoggingInterceptor()
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
 
         val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
