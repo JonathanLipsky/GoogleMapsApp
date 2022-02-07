6badda0835e9e6828d62c4031e98e66f91a3eccf
Jonathan Lipsky <lokidog2006@yahoo.com>
2022-02-07 09:03:27 -0600

    added google maps fragment and fixed layout

--- app/src/main/java/com/example/googlemapsapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/googlemapsapp/ui/home/HomeFragment.kt
@@ -6,11 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.googlemapsapp.R
 import com.example.googlemapsapp.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() , OnMapReadyCallback {
 
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var mMap: GoogleMap
     private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
@@ -25,11 +30,19 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpObserver()
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        val mapFragment = activity?.supportFragmentManager?.findFragmentById(R.id.google_map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
     }
 
     private fun setUpObserver() {
-        viewModel.text.observe(viewLifecycleOwner) {
-            binding.textHome.text = it
-        }
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
